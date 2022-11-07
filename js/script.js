ba5024fa4bbe333906fc78a8eb1affb3daf910a4
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2022-11-07 00:01:52 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ function init(){
   //Show an alert box with contents of the text field
   function nameAlert(){
    
-   //Obtain input
-	 var input = document.getElementById('entryinput').value;
-	 alert("Fatimah Syed:" + input); 
+      //Obtain input
+      var input = document.getElementById('entryinput').value;
+      alert("Fatimah Syed:" + input); 
 	 
-	 //Obtain output
-   var output = document.getElementById('textoutput');
-	 output.innerHTML = input;
+      //Obtain output
+      var output = document.getElementById('textoutput');
+      output.innerHTML = input;
  }
  
  //Configuring the button
