338feecbc1eaf4604b024b9b733e2531cf25d080
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2022-11-07 00:00:41 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function init(){
+ 
+  //Show an alert box with contents of the text field
+  function nameAlert(){
+   
+   //Obtain input
+	 var input = document.getElementById('entryinput').value;
+	 alert("Fatimah Syed:" + input); 
+	 
+	 //Obtain output
+   var output = document.getElementById('textoutput');
+	 output.innerHTML = input;
+ }
+ 
+ //Configuring the button
+ var button = document.getElementById('entrybutton');
+ button.addEventListener('click', nameAlert); 
+}
+window.addEventListener('load', init);
+
